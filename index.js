96e3de48171aadb6e88b836915da965c8897d8fd
Olivier Louvignes <olivier@mg-crea.com>
2014-08-18 17:25:49 +0200

    chore(spaces): use spaces instead of tabs

--- index.js
+++ index.js
@@ -13,173 +13,173 @@ var Readable = require('stream').Readable;
 var concat = require('./src/concat');
 
 module.exports = function(options) {
-	options = options || {};
+  options = options || {};
 
-	var startReg = /<!--\s*build:(\w+)(?:\(([^\)]+?)\))?\s+(\/?([^\s]+?))\s*-->/gim;
-	var endReg = /<!--\s*endbuild\s*-->/gim;
-	var jsReg = /<\s*script\s+.*?src\s*=\s*"([^"]+?)".*?><\s*\/\s*script\s*>/gi;
-	var cssReg = /<\s*link\s+.*?href\s*=\s*"([^"]+)".*?>/gi;
-	var startCondReg = /<!--\[[^\]]+\]>/gim;
-	var endCondReg = /<!\[endif\]-->/gim;
+  var startReg = /<!--\s*build:(\w+)(?:\(([^\)]+?)\))?\s+(\/?([^\s]+?))\s*-->/gim;
+  var endReg = /<!--\s*endbuild\s*-->/gim;
+  var jsReg = /<\s*script\s+.*?src\s*=\s*"([^"]+?)".*?><\s*\/\s*script\s*>/gi;
+  var cssReg = /<\s*link\s+.*?href\s*=\s*"([^"]+)".*?>/gi;
+  var startCondReg = /<!--\[[^\]]+\]>/gim;
+  var endCondReg = /<!\[endif\]-->/gim;
 
-	function createReadableStreamFromArray(array) {
-		var rs = new Readable({objectMode: true});
-		rs._read = function(size) {
-			array.forEach(function(item) {
-	      rs.push(item);
-	    });
-			rs.push(null);
-		};
-		return rs;
-	}
+  function createReadableStreamFromArray(array) {
+    var rs = new Readable({objectMode: true});
+    rs._read = function(size) {
+      array.forEach(function(item) {
+        rs.push(item);
+      });
+      rs.push(null);
+    };
+    return rs;
+  }
 
-	function getFiles(content, alternatePath, rootPath, basePath) {
+  function getFiles(content, alternatePath, rootPath, basePath) {
 
-		var paths = [];
-		var files = [];
+    var paths = [];
+    var files = [];
 
-		content
-			.replace(startCondReg, '')
-			.replace(endCondReg, '')
-			.replace(/<!--(?:(?:.|\r|\n)*?)-->/gim, '')
-			.replace(jsReg.test(content) ? jsReg : cssReg, function (a, b) {
-				var filePath = path.resolve(path.join(alternatePath || rootPath, b));
+    content
+      .replace(startCondReg, '')
+      .replace(endCondReg, '')
+      .replace(/<!--(?:(?:.|\r|\n)*?)-->/gim, '')
+      .replace(jsReg.test(content) ? jsReg : cssReg, function (a, b) {
+        var filePath = path.resolve(path.join(alternatePath || rootPath, b));
 
-				if (options.assetsDir) {
-					filePath = path.resolve(path.join(options.assetsDir, path.relative(basePath, filePath)));
-				}
+        if (options.assetsDir) {
+          filePath = path.resolve(path.join(options.assetsDir, path.relative(basePath, filePath)));
+        }
 
-				paths.push(filePath);
-			});
+        paths.push(filePath);
+      });
 
-		/* jshint loopfunc:true */
-		for (var i = 0, l = paths.length; i < l; ++i) {
-			var filepaths = glob.sync(paths[i]);
-			if(filepaths[0] === undefined) {
-				throw new gutil.PluginError('gulp-usemin', 'Path ' + paths[i] + ' not found!');
-			}
-			filepaths.forEach(function (filepath) {
-				files.push(new gutil.File({
-					path: filepath,
-					contents: fs.readFileSync(filepath)
-				}));
-			});
-		}
+    /* jshint loopfunc:true */
+    for (var i = 0, l = paths.length; i < l; ++i) {
+      var filepaths = glob.sync(paths[i]);
+      if(filepaths[0] === undefined) {
+        throw new gutil.PluginError('gulp-usemin', 'Path ' + paths[i] + ' not found!');
+      }
+      filepaths.forEach(function (filepath) {
+        files.push(new gutil.File({
+          path: filepath,
+          contents: fs.readFileSync(filepath)
+        }));
+      });
+    }
 
-		return createReadableStreamFromArray(files);
+    return createReadableStreamFromArray(files);
 
-	}
-
-
-	function process(files, pipelineId, index) {
-
-		var tasks = options[pipelineId] || [];
-		if(tasks.indexOf('concat') !== -1) {
-			tasks[tasks.indexOf('concat')] = concat();
-		} else if(!tasks.length || !options.skipConcat) {
-			tasks.unshift(concat());
-		}
-
-		return files
-			.pipe(multipipe.apply(multipipe, tasks))
-			.pipe(through.obj(function(file, encoding, next) {
-				file.$index = index;
-				next(null, file);
-			}));
-	}
-
-	function processHtml(htmlFile, push, next) {
-
-		var rootPath = path.dirname(htmlFile.path);
-		var sections = String(htmlFile.contents).split(endReg);
-
-		var splits = [];
-		var streams = [];
-
-		for (var i = 0, l = sections.length; i < l; ++i) {
-
-			if (sections[i].match(startReg)) {
-
-				var split = splits[i] = sections[i].split(startReg);
-				// Reset section with outer prepended content
-				sections[i] = split[0];
-				var pipelineId = split[1];
-				var alternatePath = split[2];
-				var innerContent = split[5];
-
-				/* jshint loopfunc:true */
-				var stream = process(getFiles(innerContent, alternatePath, rootPath, htmlFile.base), pipelineId, i)
-					.pipe(gutil.buffer(function(err, files) {
-
-						if(!files.length) return;
-						var index = files[0].$index;
-						// Start with reset content
-						var html = [sections[index]];
-						// html.push('<!-- built -->\n');
-
-						// Support [if] blocks
-						var startCondLine = splits[index][5].match(startCondReg);
-						var endCondLine = splits[index][5].match(endCondReg);
-						if (startCondLine && endCondLine) {
-							html.push(startCondLine[0]);
-						}
-
-						files.forEach(function(file) {
-							file.base = htmlFile.base;
-
-							var extName = path.extname(file.path);
-							var filePath = splits[index][3] || path.basename(file.path);
-							file.path = path.relative(rootPath, path.join(rootPath, filePath));
-							if (extName === '.js') {
-								html.push('<script src="' + filePath + '"></script>'); // \n
-							} else if (extName === '.css') {
-								html.push('<link rel="stylesheet" href="' + filePath + '"/>'); // \n
-							}
-							push(file);
-						});
+  }
 
 
-						if (startCondLine && endCondLine) {
-							html.push(endCondLine[0]);
-						}
+  function process(files, pipelineId, index) {
 
-						// Merge back compiled section
-						// html.push('<!-- endbuilt -->\n');
-						sections[index] = html.join('');
+    var tasks = options[pipelineId] || [];
+    if(tasks.indexOf('concat') !== -1) {
+      tasks[tasks.indexOf('concat')] = concat();
+    } else if(!tasks.length || !options.skipConcat) {
+      tasks.unshift(concat());
+    }
 
-					}));
+    return files
+      .pipe(multipipe.apply(multipipe, tasks))
+      .pipe(through.obj(function(file, encoding, next) {
+        file.$index = index;
+        next(null, file);
+      }));
+  }
 
-				streams.push(stream);
+  function processHtml(htmlFile, push, next) {
 
-			}
+    var rootPath = path.dirname(htmlFile.path);
+    var sections = String(htmlFile.contents).split(endReg);
 
-		}
+    var splits = [];
+    var streams = [];
 
-		// Pass along
-		if(!streams.length) return next(null, htmlFile);
+    for (var i = 0, l = sections.length; i < l; ++i) {
 
-		return merge(streams)
-			.pipe(gutil.buffer(function(err, files) {
-				htmlFile.contents = new Buffer(sections.join(''));
-				// Also process src html file
-				process(createReadableStreamFromArray([htmlFile]), 'html').pipe(gutil.buffer(function(err, files) {
-					next(null, files[0]);
-				}));
-   		}));
+      if (sections[i].match(startReg)) {
 
-	}
+        var split = splits[i] = sections[i].split(startReg);
+        // Reset section with outer prepended content
+        sections[i] = split[0];
+        var pipelineId = split[1];
+        var alternatePath = split[2];
+        var innerContent = split[5];
 
-	return through.obj(function(file, encoding, next) {
+        /* jshint loopfunc:true */
+        var stream = process(getFiles(innerContent, alternatePath, rootPath, htmlFile.base), pipelineId, i)
+          .pipe(gutil.buffer(function(err, files) {
+
+            if(!files.length) return;
+            var index = files[0].$index;
+            // Start with reset content
+            var html = [sections[index]];
+            // html.push('<!-- built -->\n');
+
+            // Support [if] blocks
+            var startCondLine = splits[index][5].match(startCondReg);
+            var endCondLine = splits[index][5].match(endCondReg);
+            if (startCondLine && endCondLine) {
+              html.push(startCondLine[0]);
+            }
+
+            files.forEach(function(file) {
+              file.base = htmlFile.base;
+
+              var extName = path.extname(file.path);
+              var filePath = splits[index][3] || path.basename(file.path);
+              file.path = path.relative(rootPath, path.join(rootPath, filePath));
+              if (extName === '.js') {
+                html.push('<script src="' + filePath + '"></script>'); // \n
+              } else if (extName === '.css') {
+                html.push('<link rel="stylesheet" href="' + filePath + '"/>'); // \n
+              }
+              push(file);
+            });
+
+
+            if (startCondLine && endCondLine) {
+              html.push(endCondLine[0]);
+            }
+
+            // Merge back compiled section
+            // html.push('<!-- endbuilt -->\n');
+            sections[index] = html.join('');
+
+          }));
+
+        streams.push(stream);
+
+      }
+
+    }
+
+    // Pass along
+    if(!streams.length) return next(null, htmlFile);
+
+    return merge(streams)
+      .pipe(gutil.buffer(function(err, files) {
+        htmlFile.contents = new Buffer(sections.join(''));
+        // Also process src html file
+        process(createReadableStreamFromArray([htmlFile]), 'html').pipe(gutil.buffer(function(err, files) {
+          next(null, files[0]);
+        }));
+      }));
+
+  }
+
+  return through.obj(function(file, encoding, next) {
 
     if (file.isNull()) {
       return next(null, file);
     } else if (file.isStream()) {
-			this.emit('error', new gutil.PluginError('gulp-usemin', 'Streams are not supported!'));
-			return next();
+      this.emit('error', new gutil.PluginError('gulp-usemin', 'Streams are not supported!'));
+      return next();
     }
 
-		processHtml(file, this.push.bind(this), next);
+    processHtml(file, this.push.bind(this), next);
 
-	});
+  });
 
 };
